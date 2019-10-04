2d8713a9465b03fd72c2d094eaba998ecd010527
camilo <camilo-152000@hotmail.com>
2019-10-04 11:37:33 -0500

    atributos

--- src/model/Viewer.java
+++ src/model/Viewer.java
@@ -1,5 +1,63 @@
 package model;
 
-public class Viewer {
+import java.util.Date;
 
+public class Viewer {
+	
+	private String id; 
+	private String lastName;
+	private String email;
+	private String gender;
+	private String country;
+	private Date dateBorn;
+	
+	public Viewer(String id, String lastName, String email, String gender, String country, Date dateBorn) {
+		this.id = id;
+		this.lastName = lastName;
+		this.email = email;
+		this.gender = gender;
+		this.country = country;
+		this.dateBorn = dateBorn;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public Date getDateBorn() {
+		return dateBorn;
+	}
+	public void setDateBorn(Date dateBorn) {
+		this.dateBorn = dateBorn;
+	}
+	
+	
+	
+	
 }
